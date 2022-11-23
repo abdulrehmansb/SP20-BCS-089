f8d89136a9e90d8afef0c42b74ede0c7d9f38f36
abdulrehmansb <106298624+abdulrehmansb@users.noreply.github.com>
2022-11-23 16:42:32 +0500

    lab mid

---
+++ SP20-BCS-089-LabMid/labmid.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("#firstlist").click(function () {
+    alert("my name is genius from 1st list");
+  });
+  $("#secondlist").click(function () {
+    alert("my name is genius from 2nd list");
+  });
+  $("#firstdiv").click(function () {
+    alert("dumbo from 1st div");
+  });
+  $("#seconddiv").click(function () {
+    alert("dumbo from 2nd div");
+  });
+  $("#thirddiv").click(function () {
+    alert("dumbo from 3rd div");
+  });
+});
